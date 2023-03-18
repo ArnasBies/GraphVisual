3fc5aede2a2342b181a5749639c6226ac6747568
arnukas arnukas <arnas1860@gmail.com>
2023-03-18 14:41:07 +0200

    Add project files.

---
+++ Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GraphVisual
+{
+    internal class Graph
+    {
+        //fields
+        public readonly int Width, Height;
+
+        //constructors
+        public Graph(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        //methods
+    }
+}
