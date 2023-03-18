d14ad3c10c3e5e9bd8d7b0d28f0a0a18b8b3f480
arnukas arnukas <arnas1860@gmail.com>
2023-03-18 23:23:06 +0200

    Started working on the Graph class, finished size processing and started working on the drawing itself

--- Graph.cs
+++ Graph.cs
@@ -4,21 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using NCalc.Domain;
 
 namespace GraphVisual
 {
     internal class Graph
     {
         //fields
-        public readonly int Width, Height;
+        public int Size;
+        private NCalc.Expression Function;
 
         //constructors
-        public Graph(int width, int height)
+        public Graph(int size, string function)
         {
-            Width = width;
-            Height = height;
+            Size = size;
+            Function = new NCalc.Expression(function);
+
+            if (Function.HasErrors()) Function = new("0");
         }
 
         //methods
+        public void SetNewFunction(string expression)
+        {
+            NCalc.Expression exp;
+
+            exp = new NCalc.Expression(expression);
+
+            if(!exp.HasErrors())
+            Function = exp;
+
+            else Function = new("0");
+        }
+
+        public double GetEvaluation(double x)
+        {
+            Function.Parameters["x"] = x;
+            Function.Parameters["X"] = x;
+
+
+            return (double)Function.Evaluate();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,16 +20,73 @@ namespace GraphVisual
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool GraphSizeSpecified;
+        private Graph MainGraph;
+
         public MainWindow()
         {
+            GraphSizeSpecified = false;
+            MainGraph = new(0, "0");
             InitializeComponent();
 
-            GraphFunctionBox.TextChanged += DrawGraphic;
+            GraphSizeBox.TextChanged += ProcessNewSize;
+            GraphFunctionBox.TextChanged += DrawGraph;
         }
 
-        private void DrawGraphic(object sender, TextChangedEventArgs e)
+        //Event Handlers
+        private void ProcessNewSize(object sender, TextChangedEventArgs e)
         {
-            
+            int EnteredValue;
+            float radius;
+
+            if (!int.TryParse(GraphSizeBox.Text, out EnteredValue))
+            {
+                ChangeGraphSizeIndicators();
+                MainGraph.Size = 0;
+                GraphSizeSpecified = false;
+                return;
+            }
+
+            if (EnteredValue <= 0)
+            {
+                ChangeGraphSizeIndicators();
+                MainGraph.Size = 0;
+                GraphSizeSpecified = false;
+                return;
+            }
+
+            MainGraph.Size = EnteredValue;
+            radius = (float)EnteredValue / 2;
+
+            ChangeGraphSizeIndicators(radius);
+            GraphSizeSpecified = true;
+        }
+
+        private void DrawGraph(object sender, TextChangedEventArgs e)
+        {
+            if (!GraphSizeSpecified) return;
+
+            MainGraph.SetNewFunction(GraphFunctionBox.Text);
+        }
+
+        //Utility methods
+        private bool InLimits(double x, double y) => x < MainGraph.Size * -1 || x > MainGraph.Size || y < MainGraph.Size * -1 || y > MainGraph.Size;
+
+        private void ChangeGraphSizeIndicators(float radius)
+        {
+            PosXGraphSize.Text = radius.ToString();
+            PosYGraphSize.Text = radius.ToString();
+            radius *= -1;
+            NegXGraphSize.Text = radius.ToString();
+            NegYGraphSize.Text = radius.ToString();
+        }
+
+        private void ChangeGraphSizeIndicators()
+        {
+            PosXGraphSize.Text = "?";
+            PosYGraphSize.Text = "?";
+            NegXGraphSize.Text = "?";
+            NegYGraphSize.Text = "?";
         }
     }
 }
